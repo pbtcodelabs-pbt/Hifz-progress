7b805de40f7f3d96bf84e4a26836839952dcc415
pbtcodelabs-pbt <pbtcodelabs@gmail.com>
2026-08-11 14:46:17 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hifz-progress-report-Hpr118tu1105am';
+const CACHE = 'hifz-progress-report-Hpr118tu0241pm';
 const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e=>{
