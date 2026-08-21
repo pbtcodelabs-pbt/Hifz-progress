343290aa1e05f4cc9dd2a7dfc182fd64eb3e5c79
pbtcodelabs-pbt <pbtcodelabs@gmail.com>
2026-08-21 19:32:41 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // جامعہ امتحانی سافٹ ویئر — سروس ورکر (آف لائن سپورٹ)
-const CACHE_NAME = 'jamia-hifz-cache-nastaleeq-font-v31';
+const CACHE_NAME = 'jamia-hifz-cache-nastaleeq-font-v34';
 const CORE_ASSETS = [
   './index.html',
   './manifest.json',
